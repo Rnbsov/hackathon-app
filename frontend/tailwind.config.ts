5440a4d838baec0e719bfdc2beeffc5c90cdb50d
Lavrentiy Rubtsov <rnbsov@gmail.com>
2024-08-11 16:12:25 +0500

    Revert "🎉 feat(app): changes to tailwind config"

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,22 +1,20 @@
-import type { Config } from 'tailwindcss'
-
-const withMT = require('@material-tailwind/react/utils/withMT')
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      bgprimary: '#21212b',
-      bgsecondary: '#181820'
-    }
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
-  plugins: []
-}
-
-module.exports = withMT(config)
-export default config
+  plugins: [],
+};
+export default config;
